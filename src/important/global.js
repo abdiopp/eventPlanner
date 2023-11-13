05c354d5d4cbfb24131e02bb2918e2d8cf84fcb4
Abdullah Murtaza <99031293+abdiopp@users.noreply.github.com>
2023-11-14 00:30:21 +0500

    done

--- src/important/global.js
+++ src/important/global.js
@@ -1,17 +1,15 @@
-import { toast } from "react-toastify"
+import { toast } from "react-toastify";
 
-window.getRandomId = () => Math.random().toString(36).slice(2)
-
+window.getRandomId = () => Math.random().toString(36).slice(2);
 window.toastify = (msg, type) => {
-
-    switch (type) {
-        case "success":
-            toast.success(msg)
-            break;
-        case "error":
-            toast.error(msg)
-            break;
-        default:
-            toast(msg)
-    }
-}
+  switch (type) {
+    case "success":
+      toast.success(msg);
+      break;
+    case "error":
+      toast.error(msg);
+      break;
+    default:
+      toast(msg);
+  }
+};
